380041d1eced51331cc6ddaea50a412191a68e8f
Kavya Sanil <kavyasundar05@gmail.com>
2024-07-21 11:28:31 +0400

    21 July 2024

---
+++ src/ex_07072024/Lab010.java
@@ -0,0 +1,17 @@
+package ex_07072024;
+
+public class Lab010 {
+    public static void main(String[] args) {
+
+        for(int i=0; i<=5; i++)
+        {
+            for(int j=0; j<i; j++){
+                System.out.printf("* ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
